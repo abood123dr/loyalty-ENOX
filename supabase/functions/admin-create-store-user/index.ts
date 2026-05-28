a58f9755030a241d705e89c9eed37959531698fd
abood123dr <abood123dr@gmail.com>
2026-05-29 00:39:41 +0300

    Use allowed service role secret name

--- supabase/functions/admin-create-store-user/index.ts
+++ supabase/functions/admin-create-store-user/index.ts
@@ -21,7 +21,7 @@ serve(async (req) => {
 
     const supabase = createClient(
       Deno.env.get('SUPABASE_URL')!,
-      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!,
+      Deno.env.get('SERVICE_ROLE_KEY')!,
     );
 
     const jwt = authHeader.replace('Bearer ', '');
